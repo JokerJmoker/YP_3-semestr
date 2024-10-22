95631540766f0fd1093fbf84ccb665dbd78d03ce
JokerJmoker <storos61@mail.com>
2024-10-22 18:06:14 +0300

    3.1

--- LAB_4/tsk_1/dump.py
+++ LAB_4/tsk_1/dump.py
@@ -1,11 +1,15 @@
 import pickle
 from collections import deque
+import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, 'data.pickle')
 
 iterator = iter([1, 2, 3, 4, 5])
 some_function = abs
 library_class = deque
 
-objects = [iterator, some_function, library_class] # список для сериализации
+objects = [iterator, some_function, library_class]  # список для сериализации
 
-with open('data.pickle', 'wb') as f:
+with open(file_path, 'wb') as f:
     pickle.dump(objects, f)
--- LAB_4/tsk_1/dump_read.py
+++ LAB_4/tsk_1/dump_read.py
@@ -1,24 +1,32 @@
 import pickle
+import os
 
-with open('data.pickle', 'rb') as f:
-    deserialized_objects = pickle.load(f)
-
-iterator, some_function, library_class = deserialized_objects
-
-try:
-    # проверка работы с итератором
-    print(list(iterator))
-except Exception as e:
-    print(f"Error with iterator: {e}")
+current_dir = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_dir, 'data.pickle')
 
 try:
-    # проверка работы сo встроенной функцией
-    print(some_function(-10))
-except Exception as e:
-    print(f"Error with builtin_function: {e}")
+    with open(file_path, 'rb') as f:
+        deserialized_objects = pickle.load(f)
 
-try:
-    # проверка работы с классом из библиотеки
-    print(library_class([1, 2, 3]))
-except Exception as e:
-    print(f"Error with library_class: {e}")
+    iterator, some_function, library_class = deserialized_objects
+
+    try:
+        # Проверка работы с итератором
+        print(list(iterator))
+    except Exception as e:
+        print(f"Error with iterator: {e}")
+
+    try:
+        # Проверка работы с встроенной функцией
+        print(some_function(-10))
+    except Exception as e:
+        print(f"Error with builtin_function: {e}")
+
+    try:
+        # Проверка работы с классом из библиотеки
+        print(library_class([1, 2, 3]))
+    except Exception as e:
+        print(f"Error with library_class: {e}")
+
+except FileNotFoundError:
+    print(f"File not found: {file_path}")
